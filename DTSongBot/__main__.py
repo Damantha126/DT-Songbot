feb7d7dbb445f2ffee109b1687fcce868d97426e
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-06-08 10:27:20 +0530

    DT Song Bot 🎵

---
+++ DTSongBot/__main__.py
@@ -0,0 +1,55 @@
+# SLT BrecLand <https://t.me/SLTBrecLand>
+# @Damantha_Jasinghe
+
+from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
+from DTSongBot.plugins import *
+from pyrogram import idle, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from DTSongBot import DTbot as app
+from DTSongBot import LOGGER
+
+pm_start_text = """
+Heya [{}](tg://user?id={}), I'm 🎧 Song Download bot  🎵
+Do /help for know my commands
+A bot by @Damantha_Jasinghe
+Updates channel @ankivectorUpdates
+"""
+
+help_text = """
+My commands👇
+- /song <song name>: download songs via Youtube
+- /saavn <song name>: download songs via JioSaavn
+- /deezer <song name>: download songs via Deezer
+- Send youtube url to my pm for download it on audio format
+A bot by @ankivectorUpdates
+"""
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    chat_id = message.chat.id
+    user_id = message.from_user["id"]
+    name = message.from_user["first_name"]
+    if message.chat.type == "private":
+        btn = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Updates channel", url="https://t.me/ankivectorUpdates"
+                    ),
+                    InlineKeyboardButton(
+                        text="Support Group", url="https://t.me/AnkiSupport_Official"
+                    )
+                ]
+            ]
+        )
+    else:
+        btn = None
+    await message.reply(pm_start_text.format(name, user_id), reply_markup=btn)
+
+@app.on_message(filters.command("help"))
+async def start(client, message):
+    await message.reply(help_text)
+
+app.start()
+LOGGER.info("DTSongBot is online.")
+idle()
