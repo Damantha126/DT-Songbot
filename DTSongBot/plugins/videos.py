22af0e0e01fa6fc062074bb33972e7105784ce1b
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-06-16 21:05:31 +0530

    Update videos.py

--- DTSongBot/plugins/videos.py
+++ DTSongBot/plugins/videos.py
@@ -29,7 +29,7 @@ def ytmusic(client, message):
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
+        kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
         #print(results)
         thum = mio[0]["title"]
         thumbnail = results[0]["thumbnails"][0]
